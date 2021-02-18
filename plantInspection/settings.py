22074996d5b8b09b62fa4e465f9423f8377a9a25
Vsevolod <52397981+Vsevolearn@users.noreply.github.com>
2021-02-18 14:13:57 +0300

    host1

--- plantInspection/settings.py
+++ plantInspection/settings.py
@@ -35,8 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core'
+    'core',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
